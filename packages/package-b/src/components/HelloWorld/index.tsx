2e0f4a07c461f80d2839a2ddedd05535420c031d
Roman Kulakov <roman.k@openweb.com>
2022-02-11 18:50:43 +0200

    feat: feature

--- packages/package-b/src/components/HelloWorld/index.tsx
+++ packages/package-b/src/components/HelloWorld/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './index.scss';
 
 function HelloWorld2() {
-  return <div className={styles.reallyCoolClassName}>Hello World 23</div>;
+  return <div className={styles.reallyCoolClassName}>Hello World 234</div>;
 }
 
 export { HelloWorld2 };
